a0db0eaf56406ddb67d4806fd1fa9e7dbb85bafa
jatkat45 <jat45@cornell.edu>
2014-12-06 10:51:19 -0500

    Update maps_lib.js
    Reverts to change to JT's Fusion Tables for GA

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -39,16 +39,6 @@ var MapsLib = {
   addrMarkerImage:    'images/blue-pushpin.png', // set to empty '' to hide searched address marker
   currentPinpoint:    null,
 
-// adds FusionTablesLayer (points) 
-    layer = new google.maps.FusionTablesLayer({
-        query: {
-          select: '\'Geocodable address\'',
-          from: '1mZ53Z70NsChnBMm-qEYmSDOvLXgrreLTkQUvvg'
-        }
-    });
-  
-    layer.setMap(map);
-
   initialize: function() {
     $( "#result_count" ).html("");
 
